7e32840c961bf60abf4ba5eb62c917011cd68084
ReintB <reintbarus@gmail.com>
2025-06-13 19:58:20 +0700

    feat: add login/auth and setup database

--- auth.ts
+++ auth.ts
@@ -1,9 +1,12 @@
 import NextAuth from "next-auth"
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
-  session:{ strateg:"jwt" },
+  session:{ strategy:"jwt" },
   pages:{
     signIn: "/signin",
   }
